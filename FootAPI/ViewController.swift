bf39de1b30025785c29bfd8cfdf7051b62c81377
kistalex <kistalexander@yandex.ru>
2023-04-13 16:10:13 +0400

    Initial Commit

---
+++ FootAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FootAPI
+//
+//  Created by Александр Кисть on 13.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
